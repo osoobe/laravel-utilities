9aa562f9d07225c6a6652528561333ba78944208
Oshane Bailey <b4.oshany@gmail.com>
2025-10-29 20:06:04 -0500

    close: add image helper class

---
+++ src/Helpers/ImageHelper.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace  Osoobe\Utilities\Helpers;
+
+use Illuminate\Support\Facades\Storage;
+
+
+class ImageHelper {
+
+
+    /**
+     * Store a base64 encoded image to the given path and return the file path.
+     *
+     * @param string $base64Image
+     * @param string $directory
+     * @param string|null $filename
+     * @param string $disk                  See filesystems.php
+     * @param string $visibility            private, public or null. If null, it will use the disk default visibility
+     * @param bool $dbSave                  Save image reference to database
+     * @return Image|string|false
+     */
+    public static function storeBase64Image(
+        $base64Image,
+        $directory = 'images',
+        $filename = null,
+        string $disk = 'public',
+        ?string $visibility = null
+    ) {
+        // Extract the image type and base64 data
+        if (preg_match('/^data:image\/(\w+);base64,/', $base64Image, $type)) {
+            $base64Image = substr($base64Image, strpos($base64Image, ',') + 1);
+            $extension = strtolower($type[1]);
+            if (!in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'webp'])) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        $base64Image = str_replace(' ', '+', $base64Image);
+        $imageData = base64_decode($base64Image);
+
+        if ($imageData === false) {
+            return false;
+        }
+
+        if (!$filename) {
+            $filename = uniqid('img_', true) . '.' . $extension;
+        } else {
+            // Ensure the filename has the correct extension
+            $filename = pathinfo($filename, PATHINFO_FILENAME) . '.' . $extension;
+        }
+
+        $path = $directory . '/' . $filename;
+
+        $options = [];
+        if ( !empty($visibility) ) {
+            $options['visibility'] = $visibility;
+        }
+
+        // Store the file using Laravel's storage
+        $stored = \Storage::disk($disk)
+            ->put($path, $imageData, options: $options);
+        return $stored ? $path : false;
+    }
+
+
+}
