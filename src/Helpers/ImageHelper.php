1e5789d49549ae7a1dfa81461daeaa10eea2f2b7
Oshane Bailey <b4.oshany@gmail.com>
2026-01-04 21:52:35 +0000

    close: add base64 only function

--- src/Helpers/ImageHelper.php
+++ src/Helpers/ImageHelper.php
@@ -27,17 +27,26 @@ class ImageHelper {
         ?string $visibility = null
     ) {
         // Extract the image type and base64 data
-        if (preg_match('/^data:image\/(\w+);base64,/', $base64Image, $type)) {
-            $base64Image = substr($base64Image, strpos($base64Image, ',') + 1);
-            $extension = strtolower($type[1]);
-            if (!in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'webp'])) {
-                return false;
-            }
+        // if (preg_match('/^data:image\/(\w+);base64,/', $base64Image, $type)) {
+        //     $base64Image = substr($base64Image, strpos($base64Image, ',') + 1);
+        //     $extension = strtolower($type[1]);
+        //     if (!in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'webp'])) {
+        //         return false;
+        //     }
+        // } else {
+        //     return false;
+        // }
+        // Remove the base64 prefix if it exists
+        if (str_contains($base64Image, ';base64,')) {
+            [$meta, $base64Image] = explode(';base64,', $base64Image);
+            $extension = explode('/', $meta)[1]; // e.g. image/png → png
         } else {
-            return false;
+            // Default extension if no metadata is provided
+            $extension = 'jpeg';
         }
 
-        $base64Image = str_replace(' ', '+', $base64Image);
+
+        // $base64Image = str_replace(' ', '+', $base64Image);
         $imageData = base64_decode($base64Image);
 
         if ($imageData === false) {
@@ -62,6 +71,25 @@ class ImageHelper {
         $stored = \Storage::disk($disk)
             ->put($path, $imageData, options: $options);
         return $stored ? $path : false;
+    }
+
+
+    /**
+     * Get string after ;base64,
+     *
+     * @param [type] $base64Image
+     * @return string|null
+     */
+    public static function base64Only(string $base64Image): ?string {
+        $results = explode(';base64,', $base64Image);
+
+        $len = count($results);
+        if ( $len == 1 ) {
+            return $results[0];
+        } elseif ( $len > 1 ) {
+            return $results[1];
+        }
+        return null;
     }
 
 
